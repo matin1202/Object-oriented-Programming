497b8e9da1820b691ff5359e442cd57c4b7fe959
matin1202 <matin1202@naver.com>
2022-09-20 17:30:49 +0900

    Add files via upload

---
+++ DoubleTester.java
@@ -0,0 +1,8 @@
+
+public class DoubleTester {
+	public static void main(String[] args) {
+		double probability = 8.70;
+		long percentage = Math.round(100 * probability);
+		System.out.print(percentage);
+	}
+}
